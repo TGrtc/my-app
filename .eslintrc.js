f6e590a6642bcf7ac65629a120beecf684371271
Tejas Gaikwad <tejas@Tejass-MacBook-Air.local>
2024-12-02 16:36:03 +0530

    <write your message here>

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,13 @@
 module.exports = {
   extends: ['next/core-web-vitals'],
+  plugins: ['react'],
   rules: {
-    'react/no-unescaped-entities': 'off',
+    'react/no-unescaped-entities': 0,
+    '@next/next/no-page-custom-font': 0,
   },
-} 
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    }
+  }
+}; 
